e7d26b5b6fbebdfe2d54a0426e5a05ebab476893
Soe Min <a0086845@u.nus.edu>
2017-03-23 23:58:55 +0800

    onClick for both buttons added

--- src/Tutorial2child.js
+++ src/Tutorial2child.js
@@ -27,9 +27,9 @@ function wrongText(buttonColor) {
 
 class Tutorial2child extends Component {
     state = {
-        winner: "",
         button1Color: "",
-        button2Color: ""
+        button2Color: "",
+        winner: ""
     };
 
     componentDidMount() {
@@ -39,16 +39,48 @@ class Tutorial2child extends Component {
         });
     }
 
+    handleClick1 = event => {
+        {
+            /*if button 1 is clicked*/
+        }
+        //event.preventDefault();
+        if (this.props.winner == 1) {
+            console.log("You win");
+        } else {
+            console.log("You lose");
+        }
+    };
+
+    handleClick2 = event => {
+        {
+            /*if button 1 is clicked*/
+        }
+        //event.preventDefault();
+        if (this.props.winner == 2) {
+            console.log("You win");
+        } else {
+            console.log("You lose");
+        }
+    };
+
     render() {
         return (
             <div>
                 Tutorial 2
-                <div id="button1" style={buttonColor(this.state.button1Color)}>
+                <div
+                    id="button1"
+                    style={buttonColor(this.state.button1Color)}
+                    onClick={this.handleClick1}
+                >
                     {this.props.winner == 1
                         ? this.state.button1Color
                         : wrongText(this.state.button1Color)}
                 </div>
-                <div id="button2" style={buttonColor(this.state.button2Color)}>
+                <div
+                    id="button2"
+                    style={buttonColor(this.state.button2Color)}
+                    onClick={this.handleClick2}
+                >
                     {this.props.winner == 2
                         ? this.state.button2Color
                         : wrongText(this.state.button2Color)}
